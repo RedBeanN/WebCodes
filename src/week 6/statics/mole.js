603749646a1d7afae4ad59323957860503066300
ScarHong <hongshn@mail2.sysu.edu.cn>
2015-11-15 14:15:24 +0800

    Homework 6

--- src/week 6/statics/mole.js
+++ src/week 6/statics/mole.js
@@ -1,4 +1,11 @@
 var tCtr, sCtr, timer, gameFlag;
+/*
+    这里使用全局变量是因为这些变量需要
+    在大部分的函数中共用；gameFlag是用
+    来判断游戏是否正在进行中的，可以避
+    免多次点击Start的bug，也可以避免游
+    戏结束后还能继续改变分数。
+                                        */
 
 window.onload = function() {
     document.getElementById("timer-text").value = 30;
@@ -7,9 +14,19 @@ window.onload = function() {
     gameFlag = 0;
     document.getElementById("start-button").addEventListener("click", startGame);
     document.getElementById("stop-button").addEventListener("click", stopGame);
+/*
+    初始化所有变量，同时开始监听两个按
+    钮的点击事件。
+                                        */
 }
 
 function startGame() {
+/*
+    判断是否已经开始游戏，如果已经开始
+    了，那么点击Start Game是没有任何作
+    用的；如果还没开始，重置计时器和计
+    分器。
+                                        */
     if (gameFlag === 0) {
         document.getElementById("status-text").value = "Playing";
         gameFlag = 1;
@@ -19,11 +36,6 @@ function startGame() {
         timer = window.setInterval(onTime, 1000);
         setMole();
     }
-}
-
-function gameOver() {
-    alert("Game Over.\nYour score is: " + sCtr);
-    resetMole();
 }
 
 function stopGame() {
@@ -46,7 +58,19 @@ function onTime() {
     }
 }
 
+function gameOver() {
+    alert("Game Over.\nYour score is: " + sCtr);
+    resetMole();
+}
+
 function setMole() {
+/*
+    这里包含了游戏的大部分逻辑：先清除
+    已经存在的target监听的点击事件，然
+    后为所有mole添加wrong click的点击事
+    件，最后随机选择一个点，把wrong click
+    事件改成right click事件。
+                                        */
     if (document.getElementsByClassName("target").length != 0) {
         document.getElementsByClassName("target")[0].removeEventListener("click", rightClick);
         document.getElementsByClassName("target")[0].className = "mole";
@@ -62,6 +86,12 @@ function setMole() {
 }
 
 function resetMole() {
+/*
+    在Stop Game或Game Over触发时，移除
+    所有的事件监听，然后把所有的点都重
+    置，这样点击任何一个点都不会导致分
+    数的改变。
+                                        */
     if (document.getElementsByClassName("target").length != 0) {
         document.getElementsByClassName("target")[0].className = "mole";
     }
@@ -72,6 +102,11 @@ function resetMole() {
     }
 }
 
+/*
+    如果点击是正确的，加分并重新生成一
+    个target；如果点击是错误的，扣除分
+    数，如果分数到负了始终视为0。
+                                        */
 function rightClick() {
     sCtr++;
     setMole();
